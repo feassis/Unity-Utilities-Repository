73408d97fe80d96824d24224129ca84a771ac6d7
Felipe Assis <felipe.assis.2002@gmail.com>
2025-04-02 15:58:26 -0300

    adding singleton

---
+++ Assets/Scripts/Utilities/Singleton/GenericMonoSingleton.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+namespace ServiceLocator.Utilities
+{
+    public class GenericMonoSingleton<T> : MonoBehaviour where T : GenericMonoSingleton<T>
+    {
+        private static T instance;
+        public static T Instance { get { return instance; } }
+
+        protected virtual void Awake()
+        {
+            if (instance == null)
+            {
+                instance = (T)this;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        protected virtual void OnDestroy()
+        {
+            if(instance == this)
+            {
+                instance = null;
+            }
+        }
+    }
+}
